4e93c87f53a069bda9347403d1847fb18bea185e
Vinicius Costa Predebon <kadiobaoff@gmail.com>
2023-04-26 19:55:34 -0300

    feat: Rota que busca todos os produtos

--- src/controllers/product.controllers.js
+++ src/controllers/product.controllers.js
@@ -17,3 +17,14 @@ export async function createProduct(req, res) {
         res.status(500).send(err.message)
     }
 }
+
+export async function getProducts(req, res) {
+
+    try {
+        const products = await db.collection("products").find().toArray()
+        res.status(200).send(products)
+
+    } catch (err) {
+        res.status(500).send(err.message)
+    }
+}
--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { validateSchema } from "../middlewares/validateSchema.middleware.js";
 import { productSchema } from "../schemas/product.schemas.js";
-import { createProduct } from "../controllers/product.controllers.js";
+import { createProduct, getProducts } from "../controllers/product.controllers.js";
 
 const productRouter = Router()
 
 productRouter.post("/new-product", validateSchema(productSchema), createProduct)
+productRouter.get("/get-products", getProducts)
 
 export default productRouter
