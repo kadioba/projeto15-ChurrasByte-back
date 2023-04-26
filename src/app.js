fdac0c3f89ac74ab9a2656362fc6ec88234cf330
Vinicius Costa Predebon <kadiobaoff@gmail.com>
2023-04-26 14:39:16 -0300

    Configuracao da estrutura e arquitetura

---
+++ src/app.js
@@ -0,0 +1,13 @@
+import express from "express"
+import cors from "cors"
+import router from "./routes/index.routes.js"
+import dotenv from "dotenv"
+
+dotenv.config()
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+app.use(router)
+
+app.listen(process.env.PORT, () => console.log(`Servidor rodando na porta ${process.env.PORT}`))
