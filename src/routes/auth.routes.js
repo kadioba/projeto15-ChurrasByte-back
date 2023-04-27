e969e252c25ae750254b8c8436a2f96fc4a39493
Pericles <periclesebmendes@gmail.com>
2023-04-27 15:39:04 -0300

    feat: user operations

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,10 +1,15 @@
 import { Router } from "express"
-import { validateSchema } from "../middlewares/validateSchema.middleware.js"
-import { userSchema } from "../schemas/auth.schemas.js"
-import { signUp } from "../controllers/auth.controllers.js"
+import { validateSchema } from "../middlewares/validateSchema.middleware.js";
+import { loginSchema, userSchema } from "../schemas/auth.schemas.js";
+import { login, logout, signUp } from "../controllers/auth.controllers.js";
+import { authValidation } from "../middlewares/authValidation.middleware.js";
 
-const authRouter = Router()
+const authRouter = Router();
 
-authRouter.post("/sign-up", validateSchema(userSchema), signUp)
+authRouter.post("/sign-up", validateSchema(userSchema), signUp);
+authRouter.post("/login", validateSchema(loginSchema), login);
+authRouter.post("/logout", authValidation, logout);
 
-export default authRouter
+
+
+export default authRouter;
